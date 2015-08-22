5955b58a4dcad29620dcf7bce0f9705844398f5a
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2015-08-22 20:05:53 +0300

    update docs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[![Build Status](https://travis-ci.org/arexeu/aammm.svg)](https://travis-ci.org/arexeu/aammm)
 # aammm
 Associative arrays with manual memory management
--- source/aammm.d
+++ source/aammm.d
@@ -3,7 +3,7 @@
  *
  * Copyright: Martin Nowak 2015 -.
  * License:   $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
- * Authors:   Martin Nowak
+ * Authors:   Martin Nowak, Ilya Yaroshenko (allocators support)
  */
 module aammm;
 
